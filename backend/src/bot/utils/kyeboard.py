bba18b6840584b51cfe04e46455e9abc67def9c4
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-25 00:44:36 +0300

    feat(bot): utils add reply keyboard class

--- backend/src/bot/utils/kyeboard.py
+++ backend/src/bot/utils/kyeboard.py
@@ -17,3 +17,8 @@ class Keyboard(ABC):
 class InlineKeyboard(Keyboard):
     def keyboard(self):
         return InlineKeyboardMarkup(inline_keyboard=self.buttons)
+
+
+class ReplyKeyboard(Keyboard):
+    def keyboard(self):
+        return ReplyKeyboardMarkup(keyboard=self.buttons, resize_keyboard=True, one_time_keyboard=True)
