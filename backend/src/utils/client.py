0f81d54540ef45c888b4bd54d994151728b30ed1
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-24 16:56:09 +0300

    feat(backend): utils add json client

--- backend/src/utils/client.py
+++ backend/src/utils/client.py
@@ -40,3 +40,31 @@ class Client:
             params=self.params
             )
         return response
+    
+    
+class JSONClient(Client):
+    def __init__(
+        self, 
+        base_url: str, 
+        endpoint: str ='', 
+        params: Optional[dict] = None,
+        payload: Optional[dict] = None
+        ):
+        super().__init__(
+            base_url=base_url,
+            endpoint=endpoint,
+            params=params,
+            payload=payload
+        )
+        
+    async def get(self) -> Union[list, dict, None]:
+        response = await super().get()
+        if response.status == 200:
+            return await response.json()
+        return None
+        
+    async def post(self) -> Union[list, dict, None]:
+        response = await super().post()
+        if response.status == 200:
+            return await response.json()
+        return None
