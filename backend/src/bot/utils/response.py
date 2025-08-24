86710358732c33c91d7836e0fc87475b4bb20cfa
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-25 00:49:54 +0300

    feat(bot): utils add callback response class

--- backend/src/bot/utils/response.py
+++ backend/src/bot/utils/response.py
@@ -30,3 +30,25 @@ class MessageResponse(Response):
             text=self.text,
             reply_markup=self.keyboard
         )
+
+
+class CallbackResponse(Response):
+    def __init__(
+        self, 
+        callback: CallbackQuery, 
+        text: str, 
+        keyboard: Optional[Keyboard] = None, 
+        click_text: str = "", 
+        state: Optional[FSMContext] = None):
+        self.callback = callback
+        self.keyboard = keyboard
+        self.text = text
+        self.click_text = click_text
+        self.state = state
+
+    async def response(self):
+        await self.callback.answer(self.click_text)
+        await self.callback.answer(
+            text=self.text,
+            reply_markup=self.keyboard
+        )
