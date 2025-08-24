70159d928c6ea90f73a9f600865ab796525591c4
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-24 16:55:33 +0300

    feat(backend): utils add base client class

---
+++ backend/src/utils/client.py
@@ -0,0 +1,42 @@
+from typing import Optional, Union
+from functools import wraps
+
+from aiohttp import ClientSession, ClientResponse
+
+
+class Client:
+    def __init__(
+        self, 
+        base_url: str, 
+        endpoint: str ='', 
+        params: Optional[dict] = None,
+        payload: Optional[dict] = None
+        ):
+        self.session = None
+        self.base_url = base_url
+        self.endpoint = endpoint
+        self.params = params
+        self.payload = payload
+        
+    async def open_session(self) -> None:
+        self.session = ClientSession(
+            base_url=self.base_url
+        )
+
+    async def close_session(self) -> None:
+        await self.session.close()
+        
+    async def get(self) -> ClientResponse:
+        response = await self.session.get(
+            url=self.endpoint,
+            params=self.params
+            )
+        return response
+        
+    async def post(self) -> ClientResponse:
+        response = await self.session.post(
+            url=self.endpoint, 
+            json=self.payload, 
+            params=self.params
+            )
+        return response
