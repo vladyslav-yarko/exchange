0ba709a2347abeef09f1e1ac98704133e6db2289
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-24 16:56:39 +0300

    feat(backend): utils add html client

--- backend/src/utils/client.py
+++ backend/src/utils/client.py
@@ -68,3 +68,29 @@ class JSONClient(Client):
         if response.status == 200:
             return await response.json()
         return None
+
+
+class HTMLClient(Client):
+    def __init__(
+        self, 
+        base_url: str, 
+        endpoint: str ='', 
+        params: Optional[dict] = None,
+        payload: Optional[dict] = None
+        ):
+        super().__init__(
+            base_url=base_url,
+            endpoint=endpoint,
+            params=params,
+            payload=payload
+        )
+        
+    async def get(self) -> str:
+        response = await super().get()
+        data = await response.text()
+        return data
+        
+    async def post(self) -> str:
+        response = await super().post()
+        data = await response.text()
+        return data
