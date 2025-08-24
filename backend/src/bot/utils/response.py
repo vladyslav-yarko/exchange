5d5d5791348aca05ce3dd3624c56368b96445db0
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-25 00:49:25 +0300

    feat(bot): utils add message response class

--- backend/src/bot/utils/response.py
+++ backend/src/bot/utils/response.py
@@ -11,3 +11,22 @@ class Response(ABC):
     @abstractmethod
     def __init__(self):
         pass
+
+
+class MessageResponse(Response):
+    def __init__(
+        self, 
+        message: Message, 
+        text: str = '', 
+        keyboard: Optional[Keyboard] = None, 
+        state: Optional[FSMContext] = None):
+        self.message = message
+        self.keyboard = keyboard
+        self.text = text
+        self.state = state
+
+    async def answer(self):
+        await self.message.answer(
+            text=self.text,
+            reply_markup=self.keyboard
+        )
