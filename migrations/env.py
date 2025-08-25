18523748a5357f7acbed95239359dc5809e5985b
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-25 03:15:19 +0300

    chore(migrations): configure env file

--- migrations/env.py
+++ migrations/env.py
@@ -7,9 +7,16 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from backend.src.models import (
+    Base
+)
+from backend.src.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", settings.MYSQL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
