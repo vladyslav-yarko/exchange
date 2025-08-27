1b50b47664a2300694c8f3ec43aa05951ed086be
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-27 14:02:26 +0300

    feat(backend): add crypto constants variables

---
+++ backend/src/constants/crypto.py
@@ -0,0 +1,12 @@
+CRYPTO_SYMBOLS = [
+    "BTC", "ETH", "SOL", "TON", "BNB", "ADA", "DOGE", "XRP", "DOT", "MATIC",
+    "LTC", "TRX", "AVAX", "LINK", "ATOM", "FIL", "ETC", "ICP", "APT", "HBAR",
+    "NEAR", "VET", "GRT", "QNT", "AAVE", "ALGO", "SAND", "EGLD", "XTZ", "MANA",
+    "THETA", "FTM", "RUNE", "AXS", "FLOW", "CHZ", "CAKE", "CRV", "ZIL", "ENJ",
+    "KSM", "BAT", "1INCH", "KNC", "OMG", "XEM", "ZEN", "YFI", "BNT", "SNX",
+    "DASH", "LRC", "ICX", "STORJ", "ANKR", "HOT", "SC", "CELR", "WRX", "WIN",
+    "BTT", "MTL", "CVC", "PERP", "REN", "FET", "CTSI", "SKL", "DGB", "OGN",
+    "AR", "BAND", "SXP", "DODO", "OCEAN", "REEF", "STMX", "VTHO", "CKB", "CELO",
+    "XVS", "BADGER", "LIT", "ALICE", "BETA", "QUICK", "SUPER", "TLM", "CHR", "BAKE",
+    "GALA", "UOS", "COTI", "ILV", "LPT", "PLA", "RAY", "MDX", "JOE", "MOVR"
+]
