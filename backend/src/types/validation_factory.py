f0e4b19bd9694a36e164ec39afc74603a6efef9f
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-27 14:08:45 +0300

    feat(backend): add validation factory types

---
+++ backend/src/types/validation_factory.py
@@ -0,0 +1,9 @@
+from typing import TypeVar
+
+
+TValidationPublic = TypeVar("TValidationPublic")
+TValidationBody = TypeVar("TValidationBody")
+TValidateValidationPublic = TypeVar("TValidateValidationPublic")
+TValidateValidationBody= TypeVar("TValidateValidationBody")
+TIsVerifiedValidationPublic = TypeVar("TIsVerifiedValidationPublic")
+TIsVerifiedValidationBody = TypeVar("TIsVerifiedValidationBody")
