d298ec9ae8cba43a114ec4a18f92ddc5fe746640
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-27 14:04:21 +0300

    feat(backend): add oauth2.0 constants variables

---
+++ backend/src/constants/__init__.py
@@ -0,0 +1,2 @@
+from src.constants.crypto import CRYPTO_SYMBOLS
+from src.constants.currency import CURRENCY_SYMBOLS
---
+++ backend/src/constants/oauth2.py
@@ -0,0 +1,7 @@
+GOOGLE_URL_REDIRECT = "https://accounts.google.com/o/oauth2/v2/auth"
+
+GOOGLE_REDIRECT_URI = "http://localhost:5173/auth/google"
+
+GOOGLE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/"
+
+GOOGLE_TOKEN_ENDPOINT_URl = "token"
