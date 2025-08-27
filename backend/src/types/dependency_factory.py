319a5d3c9c89adefa83535139e969bc102f315d4
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-27 14:08:10 +0300

    feat(backend): add dependency factory types

---
+++ backend/src/types/dependency_factory.py
@@ -0,0 +1,8 @@
+from typing import TypeVar
+
+
+TSchemaBody = TypeVar("TSchemaBody")
+TSchemaPublic = TypeVar("TSchemaPublic")
+TDataSchemaPublic = TypeVar("TDataSchemaPublic")
+TPhoneNumberBody = TypeVar("TPhoneNumberBody")
+TEmailBody = TypeVar("TEmailBody")
