d649c5148158e3828072dddfa745ea5fd11fb5ac
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-28 14:31:46 +0300

    feat(backend): add get symbol price method to crypto client

--- backend/src/clients/crypto.py
+++ backend/src/clients/crypto.py
@@ -11,3 +11,11 @@ class CryptoClient(JSONClient):
         super().__init__(
             base_url=URLEnum.BASE_URL.value
         )
+        
+    async def get_symbol_price(self, symbol: str) -> Optional[dict]:
+        self.endpoint = URLEnum.PRICE_ENDPOINT.value
+        self.params = {
+            "symbol": symbol
+        }
+        data = await self.get()
+        return data
