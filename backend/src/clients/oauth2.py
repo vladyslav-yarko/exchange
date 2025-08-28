4cd6380b4570c80666880f66681bcb448bab5083
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-28 14:35:41 +0300

    feat(backend): add get google data method to oauth2.0 client

--- backend/src/clients/oauth2.py
+++ backend/src/clients/oauth2.py
@@ -12,3 +12,15 @@ class Oauth2Client(JSONClient):
         super().__init__(
             base_url=GOOGLE_TOKEN_BASE_URL
         )
+        
+    async def get_google_data(self, code: str) -> Optional[dict]:
+        self.endpoint = GOOGLE_TOKEN_ENDPOINT_URl
+        self.payload = {
+            "client_id": settings.GOOGLE_CLIENT_ID,
+            "client_secret": settings.GOOGLE_CLIENT_SECRET,
+            "grant_type": "authorization_code",
+            "redirect_uri": GOOGLE_REDIRECT_URI,
+            "code": code
+        }
+        data = await self.post()
+        return data
