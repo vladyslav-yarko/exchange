34fd7ecdad2c044ed5c91f257a7f32e55533032a
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-28 14:34:21 +0300

    feat(backend): add get symbol price method to currency client

--- backend/src/clients/currency.py
+++ backend/src/clients/currency.py
@@ -12,3 +12,14 @@ class CurrencyClient(JSONClient):
         super().__init__(
             base_url=URLEnum.BASE_URL.value
         )
+        
+    async def get_symbol_price(self, symbol1: str, symbol2: str) -> Optional[dict]:
+        self.endpoint = URLEnum.PRICE_ENDPOINT.value
+        self.params = {
+            'access_key': settings.EXCHANGERATE_API_KEY,
+            'source': symbol1,
+            'currencies': symbol2
+        }
+        data = await self.get()
+        return data
+        
