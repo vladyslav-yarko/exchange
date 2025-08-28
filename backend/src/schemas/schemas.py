0b7204025ac99c0f040dc49470e2aaa8d9a70898
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-28 14:41:55 +0300

    feat(backend): add PublicSchema schema

--- backend/src/schemas/schemas.py
+++ backend/src/schemas/schemas.py
@@ -11,3 +11,13 @@ class PaginationSchema(Schema):
     count: int = Field(..., examples=[1], ge=0)
     hasNext: bool = Field(..., examples=[False])
     total: int = Field(..., examples=[1], ge=0)
+
+
+class PublicSchema(Schema):
+    id: int = Field(..., examples=[1])
+    createdAt: datetime.datetime = Field(..., examples=["2025-07-02T00:28:57.553668"])
+    updatedAt: datetime.datetime = Field(..., examples=["2025-07-02T00:28:57.553668"])
+    
+    model_config = {
+        "extra": "ignore"
+    }
