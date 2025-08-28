f380cabf563f6372f1033bfbe4055bce215f2621
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-28 14:54:31 +0300

    feat(backend): import all crypto schemas

---
+++ backend/src/schemas/crypto/__init__.py
@@ -0,0 +1,21 @@
+from src.schemas.crypto.schemas import (
+    CryptoBody, 
+    CryptoPublic, 
+    CryptoSPublic, 
+    CryptoSubscribeBody, 
+    CryptoSubscribePublic, 
+    CryptoSubscribesPublic, 
+    CryptoPricePublic, 
+    CryptoPriceBody
+)
+from src.schemas.crypto.exceptions import (
+    GetCrypto422, 
+    CreateCrypto422, 
+    UpdateCrypto422, 
+    DeleteCrypto422, 
+    GetCryptoSubscribe422, 
+    CreateCryptoSubscribe422, 
+    UpdateCryptoSubscribe422, 
+    DeleteCryptoSubscribe422, 
+    GetCryptoPrice422
+)
