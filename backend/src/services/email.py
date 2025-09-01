7c497a40c127686ef95565a3c490380b89af4c76
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-02 00:58:02 +0300

    feat(backend): add validate method to email service

--- backend/src/services/email.py
+++ backend/src/services/email.py
@@ -37,3 +37,22 @@ class EmailService(Service):
             return (422, "Cannot send verification code. Email is invalid")
         data["expirationTime"] = ValidationEnum.EXPIRE_TIME
         return [data, id]
+    
+    async def validate(self, validation_id: str, data: ValidateEmailBody) -> Union[dict, tuple[int, str]]:
+        email = data.get('email')
+        # user = self.user_repo(self.session).get_one_by_email(email)
+        # if user:
+        #     return (422, "Email has already found")
+        redis_key = f"email-{validation_id}:{email}"
+        code = await self.redis_manager.get_string_data(redis_key)
+        if not code:
+            return (400, "Validation id or email has not found")
+        code = int(code)
+        if data.get("code") != code:
+            return (422, "Invalid verification code")
+        await self.redis_manager.delete(redis_key)
+        await self.redis_manager.set_string_data(f"verified-email:{email}", email, ValidationEnum.EXPIRE_TIME.value)
+        return {
+            "email": email,
+            "verified": True
+        }
