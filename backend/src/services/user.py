64102df6ede2c2099bec721ce3d95955aa4ab051
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-03 18:36:03 +0300

    feat(backend): add user service class

---
+++ backend/src/services/user.py
@@ -0,0 +1,32 @@
+from typing import Union, Optional, Any
+import uuid
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.utils.service import Service
+from src.utils.repository import Repository, transaction
+from src.utils.password import pw_manager
+from src.utils.jwt import jwt_manager
+from src.utils.oauth2 import oauth2
+from src.utils.client import JSONClient, http_session
+from src.schemas.user import UserBody, LoginUserBody, UpdateUserBody
+from src.enums.user import RoleEnum, TokenEnum
+from src.clients import Oauth2Client
+
+
+class UserService(Service):
+    def __init__(
+        self,
+        session: AsyncSession,
+        oauth2_client: JSONClient,
+        user_repo: Repository,
+        telegram_user_repo: Repository
+    ):
+        super().__init__(session)
+        self.repo = user_repo
+        self.user_repo = user_repo
+        self.telegram_user_repo = telegram_user_repo
+        self.jwt = jwt_manager
+        self.pw = pw_manager
+        self.oauth_manager = oauth2
+        self.client = oauth2_client
