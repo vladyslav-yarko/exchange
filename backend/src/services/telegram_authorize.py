d793170089fcdbc36652176d9f30d4b2c63c00a5
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-05 01:08:26 +0300

    feat(backend): add create one method to telegram authorize service

--- backend/src/services/telegram_authorize.py
+++ backend/src/services/telegram_authorize.py
@@ -19,3 +19,11 @@ class TelegramAuthorizeService(Service):
     async def get_one(self, chat_id: int, phone_number: str) -> Optional[dict]:
         data = await self.telegram_user_repo(self.session).get_one_by_chat_id_phone_number(chat_id, phone_number)
         return data.to_dict() if data else None
+        
+    @transaction
+    async def create_one(self, data: dict) -> bool:
+        user = await self.get_one(data["chatId"], data["phoneNumber"])
+        if not user:
+            await super().create_one(data)
+            return True
+        return False
