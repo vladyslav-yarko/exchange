7afb4c16d48a2a2182ff63a8d96b682eeb9246d4
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-12 19:35:07 +0300

    feat(backend): add send method to phone number service

--- backend/src/services/phone_number.py
+++ backend/src/services/phone_number.py
@@ -21,3 +21,18 @@ class PhoneNumberService(Service):
         self.telegram_user_repo = telegram_user_repo
         self.user_repo = user_repo
         self.code_manager = code_manager
+        
+    async def send(self, data: PhoneNumberBody) -> Union[list[dict, str], tuple[int, str]]:
+        phone_number = data.get("phoneNumber")
+        user = await self.telegram_user_repo(self.session).get_one_by_phone_number(phone_number)
+        if not user:
+            return (422, "Phone number has not found. You need to authorize it in telegram @exhange_v2_bot")
+        if user.userId is not None:
+            return (422, "Phone number has already taken")
+        code = self.code_manager.make_code()
+        id = str(uuid.uuid4())
+        await self.redis_manager.set_string_data(f"phone-{id}:{phone_number}", str(code), ValidationEnum.EXPIRE_TIME.value)
+        # CELERY 
+        await bot.app.send_message(chat_id=user.chatId, text=f"Verification code: {code}")
+        data["expirationTime"] = ValidationEnum.EXPIRE_TIME
+        return [data, id]
