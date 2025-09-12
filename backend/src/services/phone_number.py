955e3784a5a670921eb07edaad8c797fce4dde68
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-12 19:35:41 +0300

    feat(backend): add validate method to phone number service

--- backend/src/services/phone_number.py
+++ backend/src/services/phone_number.py
@@ -36,3 +36,23 @@ class PhoneNumberService(Service):
         await bot.app.send_message(chat_id=user.chatId, text=f"Verification code: {code}")
         data["expirationTime"] = ValidationEnum.EXPIRE_TIME
         return [data, id]
+    
+    async def validate(self, user_id: int, validation_id: str, data: ValidatePhoneNumberBody) -> Union[dict, tuple[int, str]]:
+        phone_number = data.get("phoneNumber")
+        user = await self.telegram_user_repo(self.session).get_one_by_phone_number(phone_number)
+        if not user:
+            return (422, "Phone number has not found. You need to authorize it in telegram @exhange_v2_bot")
+        redis_key = f"phone-{validation_id}:{phone_number}"
+        code = await self.redis_manager.get_string_data(redis_key)
+        if not code:
+            return (400, "Validation id or phone number has not found")
+        code = int(code)
+        if data.get("code") != code:
+            return (422, "Invalid verification code")
+        # await self.telegram_user_repo(self.session).update_one(phone_number, userId=user_id)
+        await self.redis_manager.delete(redis_key)
+        await self.redis_manager.set_string_data(f"verified-phone:{user_id}", phone_number, ValidationEnum.EXPIRE_TIME.value)
+        return {
+            "phoneNumber": phone_number,
+            "verified": True
+        }
