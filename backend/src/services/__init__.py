45bd2f9f4dad0cbdb1c0794f3441ef267c0339f5
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-24 18:56:34 +0300

    feat(backend): import all services

---
+++ backend/src/services/__init__.py
@@ -0,0 +1,6 @@
+from src.services.user import UserService
+from src.services.crypto import CryptoService
+from src.services.currency import CurrencyService
+from src.services.email import EmailService
+from src.services.phone_number import PhoneNumberService
+from src.services.telegram_authorize import TelegramAuthorizeService
