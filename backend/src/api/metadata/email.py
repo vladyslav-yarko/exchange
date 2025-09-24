f64b158c47c1f8fda17821d4860bfe4275d848d5
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-24 19:05:33 +0300

    feat(backend): add service dep to email dependency

---
+++ backend/src/api/metadata/email.py
@@ -0,0 +1,17 @@
+from typing import Annotated, Callable, Awaitable, Optional
+
+from fastapi import Depends, HTTPException, Response, Cookie, HTTPException
+
+from src.services import EmailService
+from src.api.dependencies.db import DBSession
+from src.repositories import UserRepository
+from src.schemas.email import EmailPublic, EmailBody, ValidateEmailPublic, ValidateEmailBody, IsVerifiedEmailBody, IsVerifiedEmailPublic
+from src.api.utils.dependency_factory import DependencyFactory
+from src.enums.validation import ValidationEnum
+
+
+async def service_dep(session: DBSession) -> EmailService:
+    return EmailService(
+        session=session,
+        user_repo=UserRepository
+    )
