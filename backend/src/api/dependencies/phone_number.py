c4c7c77611123d2ff7fda7ce095a3126ee1e217b
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-29 18:09:36 +0300

    feat(backend): add service_dep to phone number dependency

---
+++ backend/src/api/dependencies/phone_number.py
@@ -0,0 +1,19 @@
+from typing import Callable, Awaitable, Optional, Annotated
+
+from fastapi import Depends, Response, Cookie, HTTPException
+
+from src.api.dependencies.db import DBSession
+from src.schemas.phone_number import PhoneNumberBody, PhoneNumberPublic, ValidatePhoneNumberBody, ValidatePhoneNumberPublic, IsVerifiedPhoneNumberBody, IsVerifiedPhoneNumberPublic
+from src.repositories import TelegramUserRepository, UserRepository
+from src.api.utils.dependency_factory import DependencyFactory
+from src.services import PhoneNumberService
+from src.enums.validation import ValidationEnum
+from src.models import User
+
+
+async def service_dep(session: DBSession) -> PhoneNumberService:
+    return PhoneNumberService(
+        session=session,
+        telegram_user_repo=TelegramUserRepository,
+        user_repo=UserRepository
+    )
