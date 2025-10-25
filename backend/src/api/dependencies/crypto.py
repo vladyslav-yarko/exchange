15268ae0b3d9046113bc3791eb89b84b763fbd56
vladyslav-yarko <vladislavv44444@gmail.com>
2025-10-25 11:51:03 +0300

    feat(backend): import all crypto dependencies for crypto

---
+++ backend/src/api/dependencies/crypto.py
@@ -0,0 +1,12 @@
+from typing import Annotated, Callable, Awaitable
+
+from fastapi import Depends, Path, HTTPException
+
+from src.api.utils.dependency_factory import DependencyFactory
+from src.api.dependencies.db import DBSession
+from src.repositories import CryptoRepository, CryptoSubscribeRepository, UserRepository
+from src.clients import CryptoClient
+from src.services import CryptoService
+from src.schemas.crypto import CryptoBody, CryptoPublic, CryptoSPublic, CryptoSubscribeBody, CryptoSubscribePublic, CryptoSubscribesPublic, CryptoPricePublic, CryptoPriceBody
+from src.models import User
+from src.utils.validation import check_upper_case
