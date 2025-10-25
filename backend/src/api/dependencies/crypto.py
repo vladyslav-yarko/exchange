196ae5abef50d8421711dd83b169eadc546d6790
vladyslav-yarko <vladislavv44444@gmail.com>
2025-10-25 11:51:35 +0300

    feat(backend): add service_dep to crypto dependencies

--- backend/src/api/dependencies/crypto.py
+++ backend/src/api/dependencies/crypto.py
@@ -10,3 +10,13 @@ from src.services import CryptoService
 from src.schemas.crypto import CryptoBody, CryptoPublic, CryptoSPublic, CryptoSubscribeBody, CryptoSubscribePublic, CryptoSubscribesPublic, CryptoPricePublic, CryptoPriceBody
 from src.models import User
 from src.utils.validation import check_upper_case
+
+
+async def service_dep(session: DBSession) -> CryptoService:
+    return CryptoService(
+        session=session,
+        crypto_client=CryptoClient(),
+        crypto_repo=CryptoRepository,
+        crypto_subscribes_repo=CryptoSubscribeRepository,
+        user_repo=UserRepository
+    )
